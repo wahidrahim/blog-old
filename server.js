160a097147f2cbdb9a712ffc93e651981c392f64
Wahid Rahim <wahidrahim@gmail.com>
2016-01-16 21:12:23 -0500

    basic idea

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var logger = require('morgan');
+var sass = require('node-sass-middleware');
+var path = require('path');
+
+var server = express();
+
+server.set('port', process.env.PORT || 1337);
+server.set('view engine', 'jade');
+
+server.use(sass({
+  src: path.join(__dirname, 'public', 'styles', 'scss'),
+  dest: path.join(__dirname, 'public', 'styles'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: '/styles'
+}));
+server.use(logger('dev'));
+server.use(express.static(path.join(__dirname, 'public')));
+
+
+server.get('/', function(req, res) {
+  res.render('index');
+});
+
+server.listen(server.get('port'), function() {
+  console.log('Listening on ' + server.get('port'));
+});
