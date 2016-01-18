997ac2f047f6a9ac687575dcbf165ac310a0fc97
Wahid Rahim <wahidrahim@gmail.com>
2016-01-18 14:23:07 -0500

    using stylus now

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var stylus = require('stylus');
 var logger = require('morgan');
 var path = require('path');
-var sass = require('node-sass-middleware');
-var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var server = express();
@@ -10,13 +10,9 @@ var server = express();
 server.set('port', process.env.PORT || 1337);
 server.set('view engine', 'jade');
 
-server.use(sass({
-  src: path.join(__dirname, 'public', 'styles', 'scss'),
-  dest: path.join(__dirname, 'public', 'styles'),
-  prefix: '/styles'
-}));
-server.use(bodyParser.urlencoded({extended: false}));
 server.use(logger('dev'));
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(stylus.middleware(path.join(__dirname, 'public')));
 server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', function(req, res) {
@@ -31,6 +27,7 @@ server.get('/', function(req, res) {
     }
   });
 });
+
 server.post('/contact', function(req, res, next) {
   // eberything goot?
   // send mail?
