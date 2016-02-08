829badd9f64aeab94b55fc8d4773851ea0976c6c
Wahid Rahim <wahidrahim@gmail.com>
2016-02-08 15:38:01 -0500

    working contact form

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -3,3 +3,39 @@ var packery = new Packery(projects, {
   itemSelector: '.project',
   gutter: 20
 });
+
+$('#contact-form').submit(function(event) {
+  event.preventDefault();
+
+  $name = $('#name')
+  $email = $('#email')
+  $message = $('#message')
+  $notice = $('<div></div>');
+
+  if ($name.val() === '' || $email.val() === '' || $message.val() === '') {
+    $notice.attr('class', 'alert alert-info alert-dismissible');
+    $notice.append('<strong>Oops!</strong> Please include your name and email along with your message :)');
+  }
+  else {
+    $notice.removeClass()
+    $.post('/contact', {
+      name: $name.val(),
+      email: $email.val(),
+      message: $message.val()
+    }, function(success) {
+      if (success === true) {
+        $notice.attr('class', 'alert alert-success alert-dismissible');
+        $notice.append('<strong>Success!</strong> message successfully sent');
+
+        $name.val('');
+        $email.val('');
+        $message.val('');
+      }
+      else {
+        $notice.attr('class', 'alert alert-warning');
+        $notice.append('<strong>Error:</strong> unable to send mail');
+      }
+    })
+  }
+  $('#contact-form').before($notice)
+});
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+//////////////////
+// DEPENDENCIES //
+//////////////////
 var express = require('express');
 var bodyParser = require('body-parser');
 var stylus = require('stylus');
 var logger = require('morgan');
 var path = require('path');
 var session = require('express-session');
+var mailer = require('nodemailer');
 
 var server = express();
 
+///////////////////
+// CONFIGURATION //
+///////////////////
 server.set('port', process.env.PORT || 1337);
 server.set('view engine', 'jade');
 
+/////////////////
+// MIDDLEWARES //
+/////////////////
 server.use(logger('dev'));
 server.use(bodyParser.urlencoded({extended: false}));
 server.use(stylus.middleware(path.join(__dirname, 'public')));
@@ -17,6 +27,7 @@ server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', function(req, res) {
   res.render('index', {
+    // TODO refactor so skills is not being sent from here
     skills: {
       js: 80,
       html: 100,
@@ -28,13 +39,27 @@ server.get('/', function(req, res) {
   });
 });
 
-server.post('/contact', function(req, res, next) {
-  // eberything goot?
-  // send mail?
+server.post('/contact', function(req, res) {
   console.log(req.body);
-  res.redirect('back');
-  //res.render('contacted');
-  //or notify in the same page/place
+
+  transporter = mailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'wrsmtp@gmail.com',
+      pass: 'df887HAf'
+    }
+  });
+
+  transporter.sendMail({
+    to: 'wahidrahim@gmail.com',
+    subject: '[CONTACT from '+req.body.name+' : '+req.body.email+']',
+    text: req.body.message
+  }, function(err, inf) {
+    if (err)
+      res.send(false);
+    else
+      res.send(true);
+  });
 });
 
 server.listen(server.get('port'), function() {
