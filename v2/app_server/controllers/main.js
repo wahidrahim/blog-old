02c06ee4253b6a35f700c7cbdcafb8ca17b5ded2
Wahid Rahim <wahidrahim@gmail.com>
2016-02-18 14:15:03 -0500

    v2 is complete

--- v2/app_server/controllers/main.js
+++ v2/app_server/controllers/main.js
@@ -1,10 +1,27 @@
+var mailer = require('nodemailer');
+
 var index = function(req, res) {
   res.render('index');
 }
 
 var contact = function(req, res) {
-  // TODO send mail
-  console.log(req.body);
+  var transporter = mailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'wrsmtp@gmail.com',
+      pass: 'df887HAf'
+    }
+  });
+
+  var sig = '---\nname: ' + req.body.name + '\nemail: ' + req.body.email;
+
+  transporter.sendMail({
+    to: 'wahidrahim@gmail.com',
+    subject: 'mail via wahidrahim.com',
+    text: req.body.message + sig
+  }, function(err, inf) {
+    res.send(err);
+  });
 }
 
 module.exports.index = index;
--- v2/public/javascripts/scripts.js
+++ v2/public/javascripts/scripts.js
@@ -6,3 +6,37 @@ $('#portfolio').imagesLoaded(function() {
     fitWidth: true
   });
 });
+
+$('#contact-form').validator().on('submit', function(e) {
+  var $name = $('#contact-form #name');
+  var $email = $('#contact-form #email');
+  var $message = $('#contact-form #message');
+  var $notice = $('<div id="mail-success" class="alert"></div>');
+
+  // invalid form
+  if (e.isDefaultPrevented()) {
+  }
+  // valid
+  else {
+    e.preventDefault();
+
+    $.post('/contact',  {
+      name: $name.val(),
+      email: $email.val(),
+      message: $message.val()
+    }, function(err) {
+      if (err) {
+        $notice.append('<strong>OOPS!</strong> something went wrong');
+        $notice.toggleClass('alert-danger').insertBefore('#contact-form');
+      }
+      else {
+        $notice.append('<strong>SUCCESS!</strong> your message has been receieved');
+        $notice.toggleClass('alert-success').insertBefore('#contact-form');
+      }
+
+      $name.val('');
+      $email.val('');
+      $message.val('');
+    });
+  }
+});
