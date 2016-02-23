9c15ec77ebaf059fcb36886944d715189e2a74b8
Wahid Rahim <wahidrahim@gmail.com>
2016-02-23 18:01:11 -0500

    added changes suggested by sam, changed gifs to videos that play on hover

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,25 @@
+$('.item').hover(function(e) {
+  if ($('video', this).get(0))
+    $('video', this).get(0).play();
+}, function(e) {
+  if ($('video', this).get(0))
+    $('video', this).get(0).pause();
+});
+
+$('.nav a').click(function(){  
+  //Toggle Class
+  $(".active").removeClass("active");      
+  $(this).closest('li').addClass("active");
+  var theClass = $(this).attr("class");
+  $('.'+theClass).parent('li').addClass('active');
+  //Animate
+  $('html, body').stop().animate({
+    scrollTop: $( $(this).attr('href') ).offset().top
+  }, 400);
+  return false;
+});
+$('.scrollTop a').scrollTop();
+
 $('#portfolio').imagesLoaded(function() {
   $('#portfolio').masonry({
     itemSelector: '.item',
