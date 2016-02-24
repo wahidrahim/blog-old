7a14806a8b502e6230e200370e6553f31e86a16a
Wahid Rahim <wahidrahim@gmail.com>
2016-02-23 23:19:21 -0500

    changed colors and banner

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -7,15 +7,9 @@ $('.item').hover(function(e) {
 });
 
 $('.nav a').click(function(){  
-  //Toggle Class
-  $(".active").removeClass("active");      
-  $(this).closest('li').addClass("active");
-  var theClass = $(this).attr("class");
-  $('.'+theClass).parent('li').addClass('active');
-  //Animate
   $('html, body').stop().animate({
     scrollTop: $( $(this).attr('href') ).offset().top
-  }, 400);
+  }, 333);
   return false;
 });
 $('.scrollTop a').scrollTop();
