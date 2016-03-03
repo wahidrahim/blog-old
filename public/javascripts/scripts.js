e733c4e39754c51eaeb17ec56980f0d12eefef89
Wahid Rahim <wahidrahim@gmail.com>
2016-03-03 15:52:33 -0500

    nav animation, social buttons, blog to-home button

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -18,10 +18,11 @@ $('.item').hover(function(e) {
 $('.nav a').click(function(){  
   $('html, body').stop().animate({
     scrollTop: $( $(this).attr('href') ).offset().top
-  }, 333);
+  });
   return false;
 });
 $('.scrollTop a').scrollTop();
+
 
 $('#contact-form').validator().on('submit', function(e) {
   var $name = $('#contact-form #name');
@@ -33,8 +34,8 @@ $('#contact-form').validator().on('submit', function(e) {
 
   // invalid form
   if (e.isDefaultPrevented()) {
-      $notice.append('<strong>Error:</strong> invalid form');
-      $notice.toggleClass('alert-danger').insertBefore('#contact-form');
+    $notice.append('<strong>Error:</strong> invalid form');
+    $notice.toggleClass('alert-danger').insertBefore('#contact-form');
   }
   // valid
   else {
@@ -61,15 +62,33 @@ $('#contact-form').validator().on('submit', function(e) {
   }
 });
 
+$('body').scrollspy({target: '#bs-navbar-collapse'})
+
 $(window).scroll(function(e) {
+
+  // shrink and grow navbar
+  if (!$('nav').hasClass('shrunk') &&
+      $(window).scrollTop() > ($('body').height() / 5)) {
+
+    shrinkNav();
+    $('nav').addClass('shrunk')
+  }
+  else if ($('nav').hasClass('shrunk') &&
+      $(window).scrollTop() < ($('body').height() / 5)) {
+
+    growNav();
+    $('nav').removeClass('shrunk');
+  }
+ 
+  // skills progress bar animation
   $('.progress').each(function(i) {
     var bar = $(this).find('.progress-bar');
 
     if ($(this).visible(true) && !$(this).hasClass('seen')) {
-      $(this).addClass('seen');
-
       var p = bar.attr('aria-valuenow');
+
       bar.css({'width': String(p + '%')});
+      $(this).addClass('seen');
     }
     else if (!$(this).hasClass('seen')) {
       bar.css({'width': '0%'});
@@ -77,3 +96,32 @@ $(window).scroll(function(e) {
   })
 });
 $(window).scroll();
+
+function shrinkNav() {
+  $('body').animate({'padding-top': '50px'});
+  $('nav').animate({'height': '50px'});
+  $('.navbar-header .navbar-brand, .nav.navbar-nav li a').animate({
+    'height': '50px',
+    'line-height': '50px'
+  });
+  $('.navbar-brand').animate({'font-size': '2em'}, function() {
+    $(this).slideUp(200, function() {
+      $(this).text('WR');
+      $(this).slideDown(200);
+    });
+  });
+}
+
+function growNav() {
+  $('body').animate({'padding-top': '75px'});
+  $('nav').animate({'height': '75px'});
+  $('.navbar-header .navbar-brand, .nav.navbar-nav li a').animate({
+    'height': '75px',
+    'line-height': '75px'
+  });
+  $('.navbar-brand').animate({'font-size': '3em'}, function() {
+    $(this).slideUp(200, function() {
+      $(this).text('Wahid Rahim');
+      $(this).slideDown(200);
+    });
+  });}
